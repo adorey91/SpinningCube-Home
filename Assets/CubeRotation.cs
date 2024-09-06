8c87bbbe40bc7db803f537940815c5b14b087cf0
Adrian Dorey <adorey91@gmail.com>
2024-09-06 08:51:09 -0300

    finished cube

--- Assets/CubeRotation.cs
+++ Assets/CubeRotation.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CubeRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int x = 90;
+    public int y = 180;
+    public int z = 25;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
     }
 }
